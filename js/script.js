08160b1853ae787d6fddca76a5c3f23f52e0ea52
MisterKyryl <99047616+MisterKyryl@users.noreply.github.com>
2022-06-19 21:29:55 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -46,9 +46,9 @@ if (isMobile.any()) {
 
 // Меню бургер
 const iconMenu = document.querySelector('.menu__icon');
-if (iconMenu){
+if (iconMenu) {
    const menuBody = document.querySelector('.menu__body');
-   iconMenu.addEventListener("click", function(e) {
+   iconMenu.addEventListener("click", function (e) {
       document.body.classList.toggle('_lock');
       iconMenu.classList.toggle('_active');
       menuBody.classList.toggle('_active');
